187c774c7dd7ea3a8edefc419a4de04032c4aa0b
Jairo Naranjo <jairo_naranjo80111@hotmail.com>
2022-04-08 11:32:48 -0500

    nodemailer inicio

---
+++ src/routes/index.js
@@ -0,0 +1,23 @@
+const {Router} = require('express');
+const router = Router()
+const nodemailer = require('nodemailer')
+
+router.post('/contactos', (req, res) => {
+    const {nombre, correo, mensaje} = req.body
+
+    contentHTML = `
+    <h1>Informacion del contacto
+    <ul> 
+      <li>Nombre: ${nombre}</li>
+      <li>Correo: ${correo}</li>
+    </ul>
+    <p>Mensaje: ${mensaje}</p>
+    </h1>
+    `
+    nodemailer.createTransport
+    console.log(contentHTML)
+    res.send("Listo")
+    
+})
+
+module.exports = router
