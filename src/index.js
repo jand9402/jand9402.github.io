0faab8ec9f624ba4c99a3de9d7c4bac6601c3f6d
Jairo Naranjo <jairo_naranjo80111@hotmail.com>
2022-04-08 11:40:29 -0500

    desmontado nodemailer porque no esta completo

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const app = express();
-const path = require('path');
+// const express = require('express');
+// const app = express();
+// const path = require('path');
 
-app.use(express.urlencoded({extended:false}))
+// app.use(express.urlencoded({extended:false}))
 
-app.use(express.json())
+// app.use(express.json())
 
-app.use(require('./routes/index.js'))
+// app.use(require('./routes/index.js'))
 
-app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'public')))
 
-app.listen(3000, () => {
-    console.log('melo')
-})
+// app.listen(3000, () => {
+//     console.log('melo')
+// })
